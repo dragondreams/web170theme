6140568d9d16079ca17996d84cfe7096c5dc4f0a
cluetrain <greenstrat67@gmail.com>
2015-05-26 16:28:34 -0700

    adds loop to the index.php

--- index.php
+++ index.php
@@ -6,7 +6,24 @@
 		 
 			<div id="main">
 			
-			
+			 <!-- Start the Loop. -->
+						 <?php if ( have_posts() ) 
+								<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+									<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small><p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+
+						 <?php while ( have_posts() ) : the_post(); ?>
+							
+									<div class="entry">
+											<?php the_content(); ?>
+									</div>
+
+							 
+							<?php endif; ?>
+ 
+						 <?php endwhile; else : ?> 
+							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p> 
+						 <?php endif; ?>
 			
 			
 			
