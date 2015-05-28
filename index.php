6790c1de406861bed57218c1b78a488b76390351
cluetrain <greenstrat67@gmail.com>
2015-05-27 17:48:37 -0700

    changes on styling, html changes on index.php

--- index.php
+++ index.php
@@ -4,19 +4,23 @@
 
 		 <div id="container">
 		 
-			<div id="main">
-			
+			<div id="sidebar"> 
+				<?php dynamic_sidebar( 'primary' ); ?> 
+			</div>
+		 
+		 
+			<div id="main"> 
 			 <!-- Start the Loop. -->
 						 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
 							<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-									<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small><p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+						<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small><p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
 									 
 
 							 
-							<?php endif; ?>
-									<?php the_excerpt('More &raquo;'); ?>
-									<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
+						<?php endif; ?>
+							<?php the_excerpt('More &raquo;'); ?>
+							<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
                
 						 <?php endwhile; ?> 
 						 
@@ -28,21 +32,11 @@
 						<?php else : ?> 
 							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p> 
 						 <?php endif; ?>
-			
-			
-			
-			
+			 
 			</div>
 			
-			<div id="sidebar">
-			
-			<?php dynamic_sidebar( 'primary' ); ?>
-		
-			
-			</div>
+			 
 		 
-		 
-		 
-		 </div>
+	</div>
 		
 <?php get_footer()?>
