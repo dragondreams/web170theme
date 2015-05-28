aa12669a1cef0a5bf193fb106d80ef539210464b
cluetrain <greenstrat67@gmail.com>
2015-05-27 17:27:32 -0700

    functions.php registers sidebars

--- functions.php
+++ functions.php
@@ -17,4 +17,38 @@ function register_custom_menu() {
 add_action('init', 'register_nav_menus');
 
 //Register_my_Sidebar//
+
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+function my_register_sidebars() {
+
+	/* Register the primary sidebar. */
+	register_sidebar(
+		array(
+			'id' => 'primary',
+			'name' => __( 'Primary Sidebar' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">',
+			'after_title' => '</h3>'
+		)
+	);
+	
+	/* Register the secondary sidebar. */
+	register_sidebar(
+		array(
+			'id' => 'secondary',
+			'name' => __( 'Secondary Sidebar' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">',
+			'after_title' => '</h3>'
+		)
+	);
+	
+	 
+	
+	
+	/* Repeat register_sidebar() code for additional sidebars. */
+}
 ?>
