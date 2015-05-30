dabf97f463296a8d449b1bd94f36f3f060f479ca
cluetrain <greenstrat67@gmail.com>
2015-05-30 16:59:18 -0700

    changes of HTML to page.php and styling to styles.css

--- page.php
+++ page.php
@@ -1,39 +1,29 @@
 <?php get_header(); ?> 
 
 <div id="container">
-					<div id="sidebar"> 
-						<?php dynamic_sidebar( 'primary' ); ?> 
-					</div>
+		<div id="sidebar"> 
+			<?php dynamic_sidebar( 'primary' ); ?> 
+		</div>
 
-<div id="main">
-				 
- 
-	<div id="main"><!--open #main --> 
-				 
+		<div id="main"> 
+			<div id="content"><!--open #main -->  
 				<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
-                        <div class="post-box">
-							<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-							 <?php the_post_thumbnail(); ?>
-							<?php the_content('More &raquo;'); ?>
-							<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
-                        </div>
-                    <?php endwhile; ?>
-                
-                         
-                
-                </div><!-- END CONTENT -->
-				
+                    <div class="post-box">
+						<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						<?php the_post_thumbnail(); ?>
+						<?php the_content('More &raquo;'); ?>
+						<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
+                     </div>
+                    <?php endwhile; ?>  
+             </div><!-- END CONTENT --> 
 						<?php else : ?> 
 							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p> 
 						<?php endif; ?>
 				
 				<div id="widget">
 					<?php dynamic_sidebar( 'secondary' ); ?>  
-				</div>
- 	
-		</div>	
-
-			 
+				</div> 
+		</div>	 
 </div>
 
 <?php get_footer(); ?>
