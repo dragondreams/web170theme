b2b3341e7bca36915eb867df99da5b321b4671b0
cluetrain <greenstrat67@gmail.com>
2015-05-30 17:04:00 -0700

    moved the title out of the content area once more --- it was not fixed earlier

--- page.php
+++ page.php
@@ -4,9 +4,9 @@
 		<div id="sidebar"> 
 			<?php dynamic_sidebar( 'primary' ); ?> 
 		</div>
-
+		<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<div id="main"> 
-			<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+			
 			<div id="content"><!--open #main -->  
 				<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
                     <div class="post-box">
