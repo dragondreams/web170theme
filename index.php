2c64daea97bc2279c5dc6ada241c22a32e614471
cluetrain <greenstrat67@gmail.com>
2015-05-30 17:35:02 -0700

    same as above

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 		 
 			<div id="content"><!--open #main --> 
 			 <!-- Start the Loop. -->
-			 <h4 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h4>
+			 
 		 
 						 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?> 
+						 <h4 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h4>
 						<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small><p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
 									 
 					<div class="post-box">
