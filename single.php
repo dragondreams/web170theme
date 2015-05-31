5c2e7bc067956f080bd92c9bb452a01cb61dba02
cluetrain <greenstrat67@gmail.com>
2015-05-30 17:37:15 -0700

    adds single.php

--- single.php
+++ single.php
@@ -3,39 +3,43 @@
 <!-- End Header -->
 
 		 <div id="container">
-		 
-			<div id="main">
+			<div id="sidebar"> 
+				<?php dynamic_sidebar( 'primary' ); ?> 
+			</div>
 			
+		 
+			<div id="content"><!--open #main --> 
 			 <!-- Start the Loop. -->
-						 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
-							<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2> 
-
-							 
-							<?php endif; ?>
-									<?php the_content('More &raquo;'); ?>
-									<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
-               
-						 <?php endwhile; ?> 
+			 
+		 
+						 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?> 
+						 <h4 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h4>
 						 
-						  
-						 
+									 
+					<div class="post-box">
+						
+						<?php the_post_thumbnail(); ?>
+						<?php the_content('More &raquo;'); ?>
+						<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
+                     </div> 
+					 <?php endwhile; ?> 
+			</div><!-- END CONTENT --> 
 						<?php else : ?> 
 							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p> 
-						 <?php endif; ?>
-			
-			<?php comments_template(); ?> 
-			
-			
+						<?php endif; ?>	 
+							 
+               
+						 
+						 
+						 <?php comments_template(); ?>
+						 
+						 
+						 <div id="widget">
+							<?php dynamic_sidebar( 'secondary' ); ?> 
+						 
+						 </div>
+			 
 			</div>
-			
-			<div id="sidebar">
-			
-			<?php get_sidebar()?>;
-			
-			</div>
-		 
-		 
-		 
-		 </div>
+			 
 		
 <?php get_footer()?>
