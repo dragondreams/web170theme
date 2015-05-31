04ac9757c9a953b55044115f79ea4ebe5ac981f2
cluetrain <greenstrat67@gmail.com>
2015-05-30 17:00:45 -0700

    relocating the titles out of the main section

--- page.php
+++ page.php
@@ -6,10 +6,11 @@
 		</div>
 
 		<div id="main"> 
+			<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 			<div id="content"><!--open #main -->  
 				<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
                     <div class="post-box">
-						<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+						
 						<?php the_post_thumbnail(); ?>
 						<?php the_content('More &raquo;'); ?>
 						<?php edit_post_link('Edit.', '<p><small>', '</small></p>'); ?>
