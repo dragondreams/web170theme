ecaff57bf309cd7bf4969a2ee63280bf3148946a
cluetrain <greenstrat67@gmail.com>
2015-06-04 16:21:23 -0700

    front-page.php

--- front-page.php
+++ front-page.php
@@ -12,12 +12,12 @@
 				<img src="<?php bloginfo('template_url'); ?>/images/free-range-chickens.jpg" alt="Hen Chickens">
 				<img src="<?php bloginfo('template_url'); ?>/images/build-coop.jpg" alt="Build Your Own">
 			</div>
-			<h2><span>Chicken Coop Chateaus<span class='spacer'></span><span class='spacer'></span>Limitless Possibilities!</span></h2>						
+			<!--<h2><span>Chicken Coop Chateaus<span class='spacer'></span><span class='spacer'></span>Limitless Possibilities!</span></h2>						
 			<div class="feature-group adjustment">
 				<a href="products.html"><img src="<?php bloginfo('template_url'); ?>/images/chapel_styleccc.jpg" alt="Product Line" /></a>
 				<a href=""><img src="<?php bloginfo('template_url'); ?>/images/community.jpg" alt="Community" /></a>
 				<a href=""><img src="<?php bloginfo('template_url'); ?>/images/free-shipping.jpg" alt="Free Shipping" /></a>
-			</div>
+			</div>-->
 		</section>
 		
 <?php get_footer()?>
